1d2c0367709aeea5c89dc76b552a5aa64c2cc1f3
David Palisse <palisse.david44@gmail.com>
2019-03-27 14:34:56 +0100

    homepage v2 done

---
+++ app/services/fetch_events_service.rb
@@ -0,0 +1,18 @@
+require 'cinema_scraper'
+
+class FetchEventsService
+  def call
+    films = CinemaScraper.new.scrape
+
+    films.each do |title, details|
+      venue = Venue.find_by!(name: details[:cinema])
+
+      event = Event.create!(
+        venue: venue,
+        name: details[:title],
+      )
+
+      # TODO event_tags
+    end
+  end
+end
---
+++ lib/cinema_scraper.rb
@@ -0,0 +1,47 @@
+require 'nokogiri'
+require 'open-uri'
+
+class CinemaScraper
+  CINEMAS = {
+    'Katorza' => 'https://nantes.maville.com/cinema/cinemam_salle_-nantes-katorza_cinema-100007859_cine.Htm',
+    'Gaumont Nantes' => 'https://nantes.maville.com/cinema/cinemam_salle_-nantes-gaumont-nantes_cinema-100006892_cine.Htm',
+    'Le Cinématographe' => 'https://nantes.maville.com/cinema/cinemam_salle_-nantes-le-cinematographe_cinema-100006872_cine.Htm'
+  }
+
+  def scrape
+    films = {}
+
+    CINEMAS.each do |cinema, url|
+      html_file = open(url).read
+      html_doc  = Nokogiri::HTML(html_file)
+
+      html_doc.search('.elmt-liste-ligne.photo .element').each do |film|
+        result_method = search_photo_and_resume("https://nantes.maville.com#{film.search('.floatL a').attribute('href').value}")
+        films[film.search('.elmt-content .elmt-titre').text] = {
+          title: film.search('.elmt-content .elmt-titre').text,
+          cinema: cinema,
+          date: film.search('li:first-child time').text.delete("\n").gsub("\r", " "),
+          photo: result_method[:photo],
+          resume: result_method[:resume],
+          url: url,
+          genre: film.search('.elmt-text a').text.split.first
+        }
+      end
+    end
+  end
+
+  private
+
+  def search_photo_and_resume(url)
+    result_method = {}
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+
+    result_method[:photo] = html_doc.search('.link-flash .photo img').first.attribute('src').value
+    result_method[:resume] = html_doc.search('.elmt-content p.elmt-text').first.text.strip.chomp
+    result_method
+  end
+end
+
+# films = CinemaScraper.new.scrape
+# p films.first
