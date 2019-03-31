ff77fec4d60c2f4fef5315a62fd34bfd7dbd3120
Patricia Martinez <patri.martinez.pena@gmail.com>
2019-03-31 22:41:22 +0200

    lost show found

--- app/services/fetch_culture_records_service.rb
+++ app/services/fetch_culture_records_service.rb
@@ -1,13 +1,17 @@
 require 'culture_api'
 
 class FetchCultureRecordsService
+  # def print_test
+  #   api_events = CultureApi.new.fetch_daily_records
+  #   return api_events
+  # end
   def call
     api_events = CultureApi.new.fetch_daily_records
-      p api_events
+    p api_events
 
     api_events.each do |api_event|
       api_venue = api_event[:venue]
-      api_event = api_event[:event]
+      event = api_event[:event]
       venue = Venue.find_or_create_by(name: api_venue[:name]) do |v|
         v.address       = api_venue[:address]
         v.url           = api_venue[:url]
@@ -16,15 +20,15 @@ class FetchCultureRecordsService
 
       Event.create!(
         venue:          venue,
-        nom:            api_event[:nom],
-        price:          api_event[:price],
-        category:       api_event[:category],
-        date:           api_event[:date],
-        beginning_hour: api_event[:beginning_hour],
-        end_hour:       api_event[:end_hour],
-        description:    api_event[:description],
-        url:            api_event[:url],
-        picture:        api_event[:picture]
+        nom:            event[:nom],
+        price:          event[:price],
+        category:       event[:category],
+        date:           event[:date],
+        beginning_hour: event[:beginning_hour],
+        end_hour:       event[:end_hour],
+        description:    event[:description],
+        url:            event[:url],
+        picture:        event[:picture]
       )
     end
   end
--- lib/culture_api.rb
+++ lib/culture_api.rb
@@ -3,8 +3,8 @@ require 'open-uri'
 
 class CultureApi
   def fetch_daily_records
-    culture_api_url   = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&rows=10&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&refine.rubrique=Culture&refine.date=#{Date.today}"
-    event_serialized  = open(culture_api_url).read
+    api_url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&rows=100&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&refine.rubrique=Culture&refine.date=#{Date.today}"
+    event_serialized  = open(api_url).read
     results           = JSON.parse(event_serialized)
     culture_records   = []
 
@@ -17,30 +17,29 @@ class CultureApi
           # p record['fields']['heure_debut']
           culture_record = {}
           culture_record[:event] = {
-            nom: record['fields']['nom'],
-            price: record['fields']['gratuit'], # => "non" pas de champ price mais dire si est gratuit ou non
-            category: record['fields']['type'],
-            date: record['fields']['date'],
+            nom:            record['fields']['nom'],
+            price:          record['fields']['gratuit'], # => "non" pas de champ price mais dire si est gratuit ou non
+            category:       record['fields']['type'],
+            date:           record['fields']['date'],
             beginning_hour: record['fields']['heure_debut'],
-            end_hour: record['fields']['heure_fin'],
-            description: record['fields']['description'],
-            url: record['fields']['lien_agenda'],
-            picture: record['fields']['media_1'],
+            end_hour:       record['fields']['heure_fin'],
+            description:    record['fields']['description'],
+            url:            record['fields']['lien_agenda'],
+            picture:        record['fields']['media_1']
           } # pas venue_id
 
           culture_record[:venue] = {
-            name: record['fields']['lieu'],
-            address: record['fields']['adresse'],
-            url: record['fields']['lieu_siteweb'],
-            phone_number: record['fields']['lieu_tel'],
+            name:         record['fields']['lieu'],
+            address:      record['fields']['adresse'],
+            url:          record['fields']['lieu_siteweb'],
+            phone_number: record['fields']['lieu_tel']
           } # pas type de etablisement, photo
           culture_records << culture_record
         end
       end
     end
+    # return fetch_daily_records
   end
 end
-
-
 # venue = Venue.create()
 # Event.create(venue: venue)
