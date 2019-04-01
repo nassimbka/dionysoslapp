3192a35bf67979e3bf662b4459630d297e51c502
David Palisse <palisse.david44@gmail.com>
2019-04-01 16:41:01 +0200

    front event index redesigned and add waiting page before results

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,3 +10,38 @@ import "bootstrap";
 $('.carousel').carousel({
   interval: false
 })
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const UlAnswerLoading = document.querySelectorAll('#ul-answer-loading li');
+
+UlAnswerLoading.forEach((li, index) => {
+  sleep(index * 1500).then(() => {
+    li.classList.replace('hide', 'display')
+  });
+});
+
+
+const tagLineLoading = document.querySelector('.tagline-index-loading')
+const impatientGif = document.querySelector('.impatient-gif');
+const tagLineEndLoading = document.getElementById('tagline-end-loading')
+const loadingWrapper = document.querySelector('.wrapper-loading')
+const resultWrapper = document.querySelector('.wrapper-index')
+
+
+sleep(UlAnswerLoading.length * 1550).then(() => {
+  tagLineLoading.classList.add('hide')
+  impatientGif.classList.add('hide')
+  tagLineEndLoading.classList.remove('hide')
+
+  sleep(2500).then(() => {
+    tagLineEndLoading.classList.add('hide')
+    UlAnswerLoading.forEach((li, index) => {
+      li.classList.replace('display', 'hide')
+    });
+    loadingWrapper.classList.add('hide')
+    resultWrapper.classList.remove('hide')
+  })
+})
