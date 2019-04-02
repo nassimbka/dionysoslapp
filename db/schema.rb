14cd37ffe4bda2e6f85afe39f92c5ce1acfb17e0
Patricia Martinez <patri.martinez.pena@gmail.com>
2019-04-02 10:23:43 +0200

    merge for track origin master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_153834) do
+ActiveRecord::Schema.define(version: 2019_04_01_071349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_03_28_153834) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo_url"
+    t.string "cinema_genre"
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
